c2263d7ca221d88068546882d8dc323d5db65af3
AviVert <avremi6@gmail.com>
2023-07-23 17:37:21 +0300

    car fuel calculator

--- TypeScript/car fuel/dist/index.js
+++ TypeScript/car fuel/dist/index.js
@@ -1,12 +1,23 @@
-var car1 = {
+"use strict";
+let car1 = {
     maker: "mazda",
-    model: "cx5",
+    model: "3",
     year: "2015",
-    milage: 500,
-    fuel_consumption: 50
+    mileage: 500,
+    fuel_consumption: 14,
 };
-function FuelCalculate() {
-    var sum = car1.milage / car1.fuel_consumption;
-    console.log(sum);
+let car2 = {
+    maker: "Toyota",
+    model: "Corola",
+    year: "2018",
+    mileage: 400,
+    fuel_consumption: 17,
+};
+function FuelCalculate(car) {
+    let sum = car.mileage / car.fuel_consumption;
+    sum = Math.round(sum);
+    return sum;
 }
-FuelCalculate();
+console.log(FuelCalculate(car1));
+console.log(FuelCalculate(car2));
+// console.log(`fuel consumption is: ` + FuelCalculate(car:car1));
--- TypeScript/car fuel/index.ts
+++ TypeScript/car fuel/index.ts
@@ -2,24 +2,38 @@ interface car{
     maker: string,
     model: string,
     year: string,
-    milage: number,
+    mileage: number,
     fuel_consumption: number,
 }
 
-let car1:car = {
+let car1:car  = {
     maker: "mazda",
-    model: "cx5",
+    model: "3",
     year: "2015",
-    milage: 500,
-    fuel_consumption: 50,
+    mileage: 500,
+    fuel_consumption: 14,
+}
 
+let car2:car  = {
+    maker: "Toyota",
+    model: "Corola",
+    year: "2018",
+    mileage: 400 ,
+    fuel_consumption: 17,
 }
 
 
-function FuelCalculate(){
-    let sum = car1.milage / car1.fuel_consumption
-    console.log(sum);
-}
+function FuelCalculate(car: car){
+    let sum = car.mileage / car.fuel_consumption;
+    sum = Math.round(sum);
+    return sum    
+}    
 
-FuelCalculate()
 
+console.log(FuelCalculate(car1));
+console.log(FuelCalculate(car2));
+
+
+
+
+// console.log(`fuel consumption is: ` + FuelCalculate(car:car1));
