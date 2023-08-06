b64aa1df6399a28a9434ead3bc01480b723d4771
AviVert <avremi6@gmail.com>
2023-08-06 17:10:24 +0300

    chess with Rook, Bishop and Queen

--- TypeScript/Class_assignment/dist/index.js
+++ TypeScript/Class_assignment/dist/index.js
@@ -15,102 +15,162 @@ class Rook {
         this.location2 = location2 !== null && location2 !== void 0 ? location2 : 1;
     }
     getLocation() {
-        console.log(`Rook Point 1 (width): ${this.location1} \nRook Point 2 (height): ${this.location2} `);
+        if (this.location1 >= 1 && this.location2 >= 1) {
+            if (this.location1 <= this.board.width && this.location2 <= this.board.height) {
+                console.log(`\nRook Location: ${this.location2},${this.location1}`);
+            }
+            else {
+                console.log(`The move is illegal`);
+            }
+        }
+        else {
+            console.log(`The move is illegal`);
+        }
     }
     goRight(right) {
-        if (this.location1 < this.board.width) {
-            let NewMove = this.location1 += right;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location1 += right;
+        this.getLocation();
     }
     goLeft(left) {
-        if (this.location1 < this.board.width) {
-            let NewMove = this.location1 -= left;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location1 -= left;
+        this.getLocation();
     }
     goUp(up) {
-        if (this.location2 < this.board.height) {
-            let NewMove = this.location2 += up;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location2 += up;
+        this.getLocation();
     }
     goDown(down) {
-        if (this.location2 < this.board.height) {
-            let NewMove = this.location2 -= down;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location2 -= down;
+        this.getLocation();
     }
 }
 class Bishop {
-    constructor(board, location3, location2) {
+    constructor(board, location3, location4) {
         this.board = board;
         this.location3 = location3 !== null && location3 !== void 0 ? location3 : 1;
-        this.location4 = location2 !== null && location2 !== void 0 ? location2 : 1;
+        this.location4 = location4 !== null && location4 !== void 0 ? location4 : 1;
     }
     getLocation() {
-        console.log(`Bishop Point 1 (width): ${this.location3} \nBishop Point 2 (height): ${this.location4} `);
-    }
-    goRight(right) {
-        if (this.location3 < this.board.width) {
-            let NewMove = this.location3 += right;
-            this.getLocation();
+        if (this.location3 >= 1 && this.location4 >= 1) {
+            if (this.location3 <= this.board.width && this.location4 <= this.board.height) {
+                console.log(`\nBishop Location: ${this.location4},${this.location3}`);
+            }
+            else {
+                console.log(`The move is illegal`);
+            }
         }
         else {
             console.log(`The move is illegal`);
         }
     }
-    goLeft(left) {
-        if (this.location3 < this.board.width) {
-            let NewMove = this.location3 -= left;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+    goRightUp(goRightUp) {
+        this.location3 += goRightUp;
+        this.location4 += goRightUp;
+        this.getLocation();
     }
-    goUp(up) {
-        if (this.location4 < this.board.height) {
-            let NewMove = this.location4 += up;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+    goLeftUp(goLeftUp) {
+        this.location3 -= goLeftUp;
+        this.location4 += goLeftUp;
+        this.getLocation();
     }
-    goDown(down) {
-        if (this.location4 < this.board.height && this.location4 != this.) {
-            let NewMove = this.location4 -= down;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+    goRightDown(goRightDown) {
+        this.location3 += goRightDown;
+        this.location4 -= goRightDown;
+        this.getLocation();
+    }
+    goLeftDown(goLeftDown) {
+        this.location4 -= goLeftDown;
+        this.location3 -= goLeftDown;
+        this.getLocation();
     }
 }
-let NewBoard = new Board(10, 10);
+class Queen {
+    constructor(board, location5, location6) {
+        this.board = board;
+        this.location5 = location5 !== null && location5 !== void 0 ? location5 : 1;
+        this.location6 = location6 !== null && location6 !== void 0 ? location6 : 1;
+    }
+    getLocation() {
+        if (this.location5 >= 1 && this.location6 >= 1) {
+            if (this.location5 <= this.board.width && this.location6 <= this.board.height) {
+                console.log(`\nQueen Location: ${this.location6},${this.location5}`);
+            }
+            else {
+                console.log(`The move is illegal`);
+            }
+        }
+        else {
+            console.log(`The move is illegal`);
+        }
+    }
+    goRightUp(goRightUp) {
+        this.location5 += goRightUp;
+        this.location6 += goRightUp;
+        this.getLocation();
+    }
+    goLeftUp(goLeftUp) {
+        this.location5 -= goLeftUp;
+        this.location6 += goLeftUp;
+        this.getLocation();
+    }
+    goRightDown(goRightDown) {
+        this.location5 += goRightDown;
+        this.location6 -= goRightDown;
+        this.getLocation();
+    }
+    goLeftDown(goLeftDown) {
+        this.location5 -= goLeftDown;
+        this.location6 -= goLeftDown;
+        this.getLocation();
+    }
+    goRight(right) {
+        this.location5 += right;
+        this.getLocation();
+    }
+    goLeft(left) {
+        this.location5 -= left;
+        this.getLocation();
+    }
+    goUp(up) {
+        this.location6 += up;
+        this.getLocation();
+    }
+    goDown(down) {
+        this.location6 -= down;
+        this.getLocation();
+    }
+}
+function chekLocation(rook, bishop) {
+    if (rook.location1 === bishop.location3 || rook.location2 === bishop.location4) {
+        console.log(`The place is occupied`);
+    }
+}
+let NewBoard = new Board(8, 8);
+console.log(`**********Board**********`);
+///**********Board**********///
 NewBoard.PrintBoard();
 let NewLocationRook = new Rook(NewBoard, 1, 1);
-let NewLocationBishop = new Bishop(NewBoard, 2, 1);
+let NewLocationBishop = new Bishop(NewBoard, 3, 1);
+let NewLocationQueen = new Queen(NewBoard, 4, 1);
+console.log(`**********Rook**********`);
 ///**********Rook**********///
 NewLocationRook.goRight(0);
 // NewLocationRook.goLeft(0);
 // NewLocationRook.goUp(0);
-NewLocationRook.goDown(4);
+// NewLocationRook.goDown(0);
+console.log(`**********Bishop**********`);
 ///**********Bishop**********///
-NewLocationBishop.goRight(0);
-// NewLocationBishop .goLeft(0);
-// NewLocationBishop .goUp(0);
-// NewLocationBishop .goDown(0);
+NewLocationBishop.goRightUp(0);
+// NewLocationBishop.goLeftUp(0);
+// NewLocationBishop.goRightDown(0);
+// NewLocationBishop.goLeftDown(0);
+console.log(`**********Queen**********`);
+///**********Queen**********///
+NewLocationQueen.goRightUp(0);
+// NewLocationQueen.goLeftUp(0);
+// NewLocationQueen.goRightDown(0);
+// NewLocationQueen.goLeftDown(0);
+// NewLocationQueen.goRight(0);
+// NewLocationQueen.goLeft(0);
+// NewLocationQueen.goUp(0);
+// NewLocationQueen.goDown(0);
--- TypeScript/Class_assignment/index.ts
+++ TypeScript/Class_assignment/index.ts
@@ -1,7 +1,6 @@
 class Board {
     width: number;
     height: number;
-
     constructor(width: number, height: number) {
         this.width = width ?? 1;
         this.height = height ?? 1;
@@ -12,58 +11,42 @@ class Board {
 }
 
 class Rook {
-   public location1: number;
+    location1: number;
     location2: number;
     board: Board;
-    static NewLocation: number;
     constructor(board: Board, location1: number, location2: number) {
         this.board = board;
         this.location1 = location1 ?? 1;
         this.location2 = location2 ?? 1;
     }
-
     getLocation() {
-
-        console.log(`Rook Point 1 (width): ${this.location1} \nRook Point 2 (height): ${this.location2} `);
+        if (this.location1 >= 1 && this.location2 >= 1) {
+            if (this.location1 <= this.board.width && this.location2 <= this.board.height) {
+                console.log(`\nRook Location: ${this.location2},${this.location1}`);
+            }
+            else {
+                console.log(`The move is illegal`);
+            }
+        }
+        else {
+            console.log(`The move is illegal`);
+        }
     }
-
     goRight(right: number) {
-        if (this.location1 < this.board.width) {
-            let NewMove = this.location1 += right;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location1 += right;
+        this.getLocation();
     }
-
     goLeft(left: number) {
-        if (this.location1 < this.board.width) {
-            let NewMove = this.location1 -= left;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location1 -= left;
+        this.getLocation();
     }
-
     goUp(up: number) {
-        if (this.location2 < this.board.height) {
-            let NewMove = this.location2 += up;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location2 += up;
+        this.getLocation();
     }
     goDown(down: number) {
-        if (this.location2 < this.board.height) {
-            let NewMove = this.location2 -= down;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location2 -= down;
+        this.getLocation();
     }
 }
 
@@ -72,79 +55,153 @@ class Bishop {
     location4: number;
     board: Board;
     static NewLocation: number;
-    constructor(board: Board, location3: number, location2: number) {
+    location1: any;
+    location2: any;
+    constructor(board: Board, location3: number, location4: number) {
         this.board = board;
         this.location3 = location3 ?? 1;
-        this.location4 = location2 ?? 1;
+        this.location4 = location4 ?? 1;
     }
-
     getLocation() {
-
-        console.log(`Bishop Point 1 (width): ${this.location3} \nBishop Point 2 (height): ${this.location4} `);
+        if (this.location3 >= 1 && this.location4 >= 1) {
+            if (this.location3 <= this.board.width && this.location4 <= this.board.height) {
+                console.log(`\nBishop Location: ${this.location4},${this.location3}`);
+            }
+            else {
+                console.log(`The move is illegal`);
+            }
+        }
+        else {
+            console.log(`The move is illegal`);
+        }
     }
-
+    goRightUp(goRightUp: number) {
+        this.location3 += goRightUp;
+        this.location4 += goRightUp;
+        this.getLocation();
+    }
+    goLeftUp(goLeftUp: number) {
+        this.location3 -= goLeftUp;
+        this.location4 += goLeftUp;
+        this.getLocation();
+    }
+    goRightDown(goRightDown: number) {
+        this.location3 += goRightDown;
+        this.location4 -= goRightDown;
+        this.getLocation();
+    }
+    goLeftDown(goLeftDown: number) {
+        this.location4 -= goLeftDown;
+        this.location3 -= goLeftDown;
+        this.getLocation();
+    }
+    
+}
+class Queen {
+    location5: number;
+    location6: number;
+    board: Board;
+    constructor(board: Board, location5: number, location6: number) {
+        this.board = board;
+        this.location5 = location5 ?? 1;
+        this.location6 = location6 ?? 1;
+    }
+    getLocation() {
+        if (this.location5 >= 1 && this.location6 >= 1) {
+            if (this.location5 <= this.board.width && this.location6 <= this.board.height) {
+                console.log(`\nQueen Location: ${this.location6},${this.location5}`);
+            }
+            else {
+                console.log(`The move is illegal`);
+            }
+        }
+        else {
+            console.log(`The move is illegal`);
+        }
+    }
+    goRightUp(goRightUp: number) {
+        this.location5 += goRightUp;
+        this.location6 += goRightUp;
+        this.getLocation();
+    }
+    goLeftUp(goLeftUp: number) {
+        this.location5 -= goLeftUp;
+        this.location6 += goLeftUp;
+        this.getLocation();
+    }
+    goRightDown(goRightDown: number) {
+        this.location5 += goRightDown;
+        this.location6 -= goRightDown;
+        this.getLocation();
+    }
+    goLeftDown(goLeftDown: number) {
+        this.location5 -= goLeftDown;
+        this.location6 -= goLeftDown;
+        this.getLocation();
+    }
     goRight(right: number) {
-        if (this.location3 < this.board.width ) {
-            let NewMove = this.location3 += right;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location5 += right;
+        this.getLocation();
     }
-
     goLeft(left: number) {
-        if (this.location3 < this.board.width) {
-            let NewMove = this.location3 -= left;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location5 -= left;
+        this.getLocation();
     }
-
     goUp(up: number) {
-        if (this.location4 < this.board.height) {
-            let NewMove = this.location4 += up;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location6 += up;
+        this.getLocation();
     }
     goDown(down: number) {
-        if (this.location4 < this.board.height && this.location4 != this.) {
-            let NewMove = this.location4 -= down;
-            this.getLocation();
-        }
-        else {
-            console.log(`The move is illegal`);
-        }
+        this.location6 -= down;
+        this.getLocation();
     }
-
 }
-let NewBoard = new Board(10, 10);
 
+function chekLocation(rook: Rook, bishop: Bishop) {
+    if (rook.location1 === bishop.location3 || rook.location2 === bishop.location4) {
+        console.log(`The place is occupied`);
+    }
+}
+
+let NewBoard = new Board(8, 8);
+console.log(`**********Board**********`);
+
+///**********Board**********///
 NewBoard.PrintBoard();
 
 let NewLocationRook = new Rook(NewBoard, 1, 1);
-let NewLocationBishop = new Bishop(NewBoard, 2, 1);
+let NewLocationBishop = new Bishop(NewBoard, 3, 1);
+let NewLocationQueen = new Queen(NewBoard, 4, 1);
 
-
+console.log(`**********Rook**********`);
 ///**********Rook**********///
 
 NewLocationRook.goRight(0);
 // NewLocationRook.goLeft(0);
 // NewLocationRook.goUp(0);
-NewLocationRook.goDown(4);
+// NewLocationRook.goDown(0);
 
-
+console.log(`**********Bishop**********`);
 ///**********Bishop**********///
 
-NewLocationBishop.goRight(0);
-// NewLocationBishop .goLeft(0);
-// NewLocationBishop .goUp(0);
-// NewLocationBishop .goDown(0);
+NewLocationBishop.goRightUp(0);
+// NewLocationBishop.goLeftUp(0);
+// NewLocationBishop.goRightDown(0);
+// NewLocationBishop.goLeftDown(0);
+
+console.log(`**********Queen**********`);
+///**********Queen**********///
+
+NewLocationQueen.goRightUp(0);
+// NewLocationQueen.goLeftUp(0);
+// NewLocationQueen.goRightDown(0);
+// NewLocationQueen.goLeftDown(0);
+
+// NewLocationQueen.goRight(0);
+// NewLocationQueen.goLeft(0);
+// NewLocationQueen.goUp(0);
+// NewLocationQueen.goDown(0);
+
 
 
 
