21d6ccc32f93b94ec47b715a377e8407c4fe6e13
chagitver <gochagi@telem.openu.ac.il>
2023-09-27 00:16:13 +0300

    trivia 0.1

--- TypeScript/Quiz/dist/index.js
+++ TypeScript/Quiz/dist/index.js
@@ -1,22 +1,5 @@
 "use strict";
-let easybutton = document.querySelector('.easy');
-let mediumbutton = document.querySelector('.medium');
-let hardbutton = document.querySelector('.hard');
-easybutton.addEventListener('click', () => {
-    easybutton.style.backgroundColor = 'red';
-    mediumbutton.style.backgroundColor = '#4070F4';
-    hardbutton.style.backgroundColor = '#4070F4';
-});
-mediumbutton.addEventListener('click', () => {
-    easybutton.style.backgroundColor = '#4070F4';
-    mediumbutton.style.backgroundColor = 'red';
-    hardbutton.style.backgroundColor = '#4070F4';
-});
-hardbutton.addEventListener('click', () => {
-    easybutton.style.backgroundColor = '#4070F4';
-    mediumbutton.style.backgroundColor = '#4070F4';
-    hardbutton.style.backgroundColor = 'red';
-});
+let SelectLevel = document.querySelector('#level');
 class Question {
     constructor(questionID, question, option1, option2, option3, option4) {
         this.questionID = questionID;
@@ -27,31 +10,41 @@ class Question {
         this.option4 = option4;
     }
 }
-let questionID = +1;
+let questionID = 1;
 let questionsEasy = [];
 let questionsMedium = [];
 let questionsHard = [];
 function AddQuestion(event) {
     event.preventDefault();
-    let question = document.querySelector('.question');
-    let questionVal = question.value;
-    let option1 = document.querySelector('.option1');
-    let option1Val = option1 === null || option1 === void 0 ? void 0 : option1.value;
-    let option2 = document.querySelector('.option2');
-    let option2Val = option2 === null || option2 === void 0 ? void 0 : option2.value;
-    let option3 = document.querySelector('.option3');
-    let option3Val = option3 === null || option3 === void 0 ? void 0 : option3.value;
-    let option4 = document.querySelector('.option4');
-    let option4Val = option4 === null || option4 === void 0 ? void 0 : option4.value;
+    let question = document.querySelector('.question').value;
+    let option1 = document.querySelector('.option1').value;
+    let option2 = document.querySelector('.option2').value;
+    let option3 = document.querySelector('.option3').value;
+    let option4 = document.querySelector('.option4').value;
     let newQuestion = {
-        questionID: questionID + 1,
-        questionVal: question,
-        option1Val: option1,
-        option2Val: option2,
-        option3Val: option3,
-        option4Val: option4,
+        questionID: questionID++,
+        question,
+        option1,
+        option2,
+        option3,
+        option4,
     };
-    localStorage.setItem("questionsEasy", JSON.stringify(questionsEasy));
+    let selectedValue = SelectLevel.value;
+    if (selectedValue == 'easy') {
+        console.log('easy');
+        questionsEasy.push(newQuestion);
+        localStorage.setItem("questionsEasy", JSON.stringify(questionsEasy));
+    }
+    else if (selectedValue == 'medium') {
+        console.log('medium');
+        questionsMedium.push(newQuestion);
+        localStorage.setItem("questionsMedium", JSON.stringify(questionsMedium));
+    }
+    else if (selectedValue == 'hard') {
+        console.log('hard');
+        questionsHard.push(newQuestion);
+        localStorage.setItem("questionsHard", JSON.stringify(questionsHard));
+    }
     console.log(newQuestion);
 }
 const myform = document.querySelector('.Myform');
--- TypeScript/Quiz/index.ts
+++ TypeScript/Quiz/index.ts
@@ -1,27 +1,10 @@
-let easybutton = document.querySelector('.easy') as HTMLElement;
-let mediumbutton = document.querySelector('.medium') as HTMLElement;
-let hardbutton = document.querySelector('.hard') as HTMLElement;
+let SelectLevel = document.querySelector('#level') as HTMLSelectElement;
 
-easybutton.addEventListener('click', () => {
-    easybutton.style.backgroundColor = 'red';
-    mediumbutton.style.backgroundColor = '#4070F4';
-    hardbutton.style.backgroundColor = '#4070F4';
-})
-mediumbutton.addEventListener('click', () => {
-    easybutton.style.backgroundColor = '#4070F4';
-    mediumbutton.style.backgroundColor = 'red';
-    hardbutton.style.backgroundColor = '#4070F4';
-})
-hardbutton.addEventListener('click', () => {
-    easybutton.style.backgroundColor = '#4070F4';
-    mediumbutton.style.backgroundColor = '#4070F4';
-    hardbutton.style.backgroundColor = 'red';
-})
 class Question {
     constructor(public questionID: number, public question: string, public option1: string, public option2: string, public option3: string, public option4: string) {
     }
 }
-let questionID = +1;
+let questionID = 1;
 
 let questionsEasy: Question[] = [];
 let questionsMedium: Question[] = [];
@@ -29,27 +12,36 @@ let questionsHard: Question[] = [];
 
 function AddQuestion(event: Event) {
     event.preventDefault();
-    let question = document.querySelector('.question') as HTMLTextAreaElement;
-    let questionVal = question.value;
-    let option1 = document.querySelector('.option1') as HTMLTextAreaElement;
-    let option1Val = option1?.value;
-    let option2 = document.querySelector('.option2') as HTMLTextAreaElement;
-    let option2Val = option2?.value;
-    let option3 = document.querySelector('.option3') as HTMLTextAreaElement;
-    let option3Val = option3?.value;
-    let option4 = document.querySelector('.option4') as HTMLTextAreaElement;
-    let option4Val = option4?.value;
+    let question = (document.querySelector('.question') as HTMLTextAreaElement).value;
+    let option1 = (document.querySelector('.option1') as HTMLTextAreaElement).value;
+    let option2 = (document.querySelector('.option2') as HTMLTextAreaElement).value;
+    let option3 = (document.querySelector('.option3') as HTMLTextAreaElement).value;
+    let option4 = (document.querySelector('.option4') as HTMLTextAreaElement).value;
 
     let newQuestion: Question = {
-        questionID: questionID + 1,
-        questionVal: question,
-        option1Val: option1,
-        option2Val: option2,
-        option3Val: option3,
-        option4Val: option4,
+        questionID: questionID ++,
+        question,
+        option1,
+        option2,
+        option3,
+        option4,
     }
-    localStorage.setItem("questionsEasy", JSON.stringify(questionsEasy));
-
+        let selectedValue = SelectLevel.value
+        if(selectedValue == 'easy'){
+            console.log('easy');
+            questionsEasy.push(newQuestion);
+            localStorage.setItem("questionsEasy", JSON.stringify(questionsEasy));
+        }
+        else if(selectedValue == 'medium'){
+            console.log('medium');
+            questionsMedium.push(newQuestion);
+            localStorage.setItem("questionsMedium", JSON.stringify(questionsMedium));
+        }
+        else if(selectedValue == 'hard'){
+            console.log('hard');
+            questionsHard.push(newQuestion);
+            localStorage.setItem("questionsHard", JSON.stringify(questionsHard));
+        }
     console.log(newQuestion);
 
 }
