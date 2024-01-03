17c797ffaa65171dc0e61b034b9eb899113accec
Aviver100 <avi.vertsman1@gmail.com>
2024-01-03 20:40:37 +0200

    edit ts file

--- Node/src/app.ts
+++ Node/src/app.ts
@@ -1,12 +1,34 @@
-import http from 'http';
-import fs from 'fs';
+import { log } from 'console';
 import express from 'express';
-
-const port = 4000;
 const app = express();
+const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.json());
 
-app.listen(port, () =>{
-console.log(`my server: ${port}`);
+interface product {
+    name: string,
+    price: number
+}
+
+const Products:product[] = [];
+
+app.post("/api/product", (req, res)=>{
+    const newProduct:product = req.body;
+    const index = Products.findIndex(item =>{newProduct.name === item.name})
+    if(!index){
+        Products.push(newProduct);
+        console.log(newProduct);
+        res.send({Products})
+    }else{
+        throw (`the name ${newProduct.name} in use`)
+    // }
 })
+ 
+app.get("/api/products", (req,res)=>{
+    res.send(Products)
+})
+app.listen(port, ()=>{
+    console.log(`server http://loclhost/${port} run`);
+    
+})
+
