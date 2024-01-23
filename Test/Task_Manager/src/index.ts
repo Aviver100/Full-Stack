8a242868b822b840e9190990574b71a87decb500
Aviver100 <avi.vertsman1@gmail.com>
2024-01-23 17:13:41 +0200

    v2

--- Test/Task_Manager/src/index.ts
+++ Test/Task_Manager/src/index.ts
@@ -1,8 +1,7 @@
 import express from 'express';
-import {DeleteTask, GetTask, GetTasks, UpdateTask, creatTask} from './controllers/index'
-import {Tasks} from './controllers/index'
-import { addTasValidation } from './validation/taskFormValidation';
-import { checkExact, oneOf } from 'express-validator';
+import { DeleteTask, GetTask, GetTasks, UpdateTask, creatTask } from './controllers/index'
+import { Tasks } from './controllers/index'
+import { addTaskValidation } from './validation/taskFormValidation';
 
 const app = express();
 const port = 3000;
@@ -12,15 +11,15 @@ app.use(express.urlencoded());
 app.use(express.static('public'));
 
 app.get('/api/tasks', (req, res) => {
-    res.send({Tasks});
+    res.send({ Tasks });
 });
 
-app.post('/api/add-task', oneOf(addTasValidation), creatTask);
+app.post('/api/add-task', addTaskValidation, creatTask);
 app.get('/api/:id', GetTask);
 app.patch('/api/:id', UpdateTask);
 app.delete('/api/:id', DeleteTask);
 app.get('/api/tasks', GetTasks);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`); 
+    console.log(`Server is running on http://localhost:${port}`);
 });
