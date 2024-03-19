49d0c44f171f33ec9be4ada36f3a9ce19d28dbac
Aviver100 <avi.vertsman1@gmail.com>
2024-03-19 20:05:58 +0200

    start to add remove button

--- React/Products/products/src/App.tsx
+++ React/Products/products/src/App.tsx
@@ -1,27 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Box from './Box/Box'
-import { products } from './products';
+import Box from './Box/productBox'
+
 
 function App() {
   // const [count, setCount] = useState(0)
-
+  // debugger
   return (
     <>
-      {products.map((product) => (
-        <Box key={product.id}>
-          <img src={product.images[1]} alt="product image" />
-          <h4>{product.title}</h4>
-          <h6>{product.description}</h6>
-          <p>Brand: {product.brand}</p><br />
-          <p>Price: {product.price}</p>
-          <p>Rating: {product.rating}</p>
-          <p>Stock: {product.stock}</p>
-
-        </Box>
-      ))}
+      <Box/>
     </>
   )
 }
