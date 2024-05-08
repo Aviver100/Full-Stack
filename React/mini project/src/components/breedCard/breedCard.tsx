1804cc937933aa398c520c8286b9407b69a3e5e2
Aviver100 <avi.vertsman1@gmail.com>
2024-05-08 12:15:37 +0300

    fix breed photos

--- React/mini project/src/components/breedCard/breedCard.tsx
+++ React/mini project/src/components/breedCard/breedCard.tsx
@@ -2,41 +2,23 @@ import { useEffect, useState } from 'react'
 import './breedCard.scss'
 
 
-function breedCard() {
-    const [breeds, setBreeds] = useState([]);
-    const [photos, setPhotos] = useState({});
+function breedCard({ breed }: { breed: string }) {
+    const [breedPhoto, setBreedPhoto] = useState('');
+
 
     useEffect(() => {
-        fetch('https://dog.ceo/api/breeds/list/all')
-            .then(response => response.json())
-            .then(data => setBreeds(data.message))
-            .catch(err => console.error(err))
-    }, []);
+        fetch(`https://dog.ceo/api/breed/${breed}/images/random/1`)
+            .then(data => data.json())
+            .then(json => setBreedPhoto(json.message))
+            .catch(() => alert())
+    }, [breed])
 
-    useEffect(() => {
-        if (breeds.length > 0) {
-            breeds.forEach(breed => {
-                fetch(`https://dog.ceo/api/breed/${breed}/images/random/1`)
-                    .then(response => response.json())
-                    .then(data => {
-                        setPhotos(prevPhotos => ({
-                            ...prevPhotos,
-                            [breed]: data.message[0],
-                        }));
-                    })
-                    .catch(err => console.error(err));
-            });
-        }
-    }, [breeds]);
     return (
         <>
-        {
-            breeds.map(breed =>{
             <div key={breed} className="breedCard">
+                <img src={breedPhoto} />
                 <p>{breed}</p>
             </div>
-            })
-        }
         </>
     )
 }
--- React/mini project/src/components/mainPage/mainPage.tsx
+++ React/mini project/src/components/mainPage/mainPage.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import './mainPage.scss'
 import BreedCard from '../breedCard/breedCard';
 
-
 function mainPage() {
     const [breed, setBreed] = useState([]);
     const [photo, setPhoto] = useState([]);
@@ -15,21 +14,16 @@ function mainPage() {
             .catch(err => console.error(err))
     }, []);
 
-    fetch(`https://dog.ceo/api/breed/${breed}/images/random/1`)
-    //     .then(response => response.json())
-    //     .then(data => setPhoto(data.message))
-    //     .catch(err => console.error(err))
-
     return (
         <>
             <input className='search' type="text" />
             <div className="main">
-                {Object.keys(breed).map((breed) => (
-                    <BreedCard breed={breed} /> 
-                    // <BreedCard /> 
-
-                ))
-                }
+                <>
+                    {Object.keys(breed).map((breed) => (
+                        <BreedCard key={breed} breed={breed} />
+                    ))
+                    }
+                </>
             </div>
         </>
     )
